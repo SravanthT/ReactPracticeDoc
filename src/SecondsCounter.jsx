2fdbf4719d42badb85cede7318110799513299b1
SravanthT <83528948+SravanthT@users.noreply.github.com>
2021-10-27 23:46:05 +0530

    Added setIntervel in secondsCounter added setintervel callback function which will update Time every second and render's it.

--- src/SecondsCounter.jsx
+++ src/SecondsCounter.jsx
@@ -1,29 +1,37 @@
-import React from "react"
+import React from "react";
 class Timer extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {secs:0}
+    this.state = { secs: 0 };
   }
-  tick(){
-    this.setState(state => ({
-      secs: state.secs+1
-    }))
+  tick() {
+    this.setState((state) => ({
+      secs: state.secs + 1
+    }));
   }
 
-  componentDidMount(){
-    this.interval=setInterval(() => this.tick(),1000 );
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), Trick(), 1000);
   }
-  componentWillUnmount(){
+  componentWillUnmount() {
     clearInterval(this.interval);
   }
-  render(){
-    return(
+  render() {
+    return (
       <div>
-          Seconds: {this.state.secs}
-        </div>
-
-    )
+        Seconds: {this.state.secs}
+        <Trick />
+      </div>
+    );
   }
+}
+function Trick() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2> It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
 }
 
 export default Timer;
